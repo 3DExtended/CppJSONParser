38da0b07e4e602b6a46f98aaf1e6c72b330f2972
Peter Esser <peter.esser@uhs-computer.de>
2018-12-28 11:24:06 +0100

    WIP Found how to rewire pointers...

--- example/Example.cc
+++ example/Example.cc
@@ -7,21 +7,21 @@
 #include <functional>
 #include <CppJSONParser.hh>
 
-class Object1 : public CJP::Base
+class Object : public CJP::Base
 {
-    REGISTER(Object1);
+    REGISTER(Object);
 
   public:
-    Object1(){};
-    virtual ~Object1(){};
+    Object(){};
+    virtual ~Object(){};
     virtual void foo() { std::cout << "1"; };
-    std::string name;
-    std::string objPath;
-    std::string material;
+    std::string* name;
+    std::string* objPath;
+    std::string* material;
 };
-REGISTERIMPL(Object1);
+REGISTERIMPL(Object);
 
-class Object2 : public CJP::Base
+/*class Object2 : public CJP::Base
 {
   private:
     static const CreatorImpl<Object2> creator;
@@ -35,20 +35,19 @@ class Object2 : public CJP::Base
     std::string objPath;
     std::string material;
 };
-const CJP::CreatorImpl<Object2> Object2::creator("Object2");
+const CJP::CreatorImpl<Object2> Object2::creator("Object2");*/
 
 int main(int argc, char const *argv[])
 {
-    Base *p;
-    p = Factory::create("Object2");
-    std::cout << p->name;
-    /*auto parser = new CJP::JSONParser();
+    //Base *p;
+    //p = Factory::create("Object2");
+
+    auto parser = new CJP::JSONParser();
     parser->registerType(
         "Object",
         CJP::SpecialFields::Name, &Object::name,
         "objPath", &Object::objPath,
-        "material", &Object::material
-    );
+        "material", &Object::material);
 
     parser->add("Object");
     //parser->register("objects", std::vector<Object>);//*/
--- include/CppJSONParser.hh
+++ include/CppJSONParser.hh
@@ -8,8 +8,11 @@
 #include "CreatorImpl.hh"
 #include "FactoryPattern.hh"
 
+#define customoffsetof(s, m) (reinterpret_cast<long>(&(reinterpret_cast<s const volatile *>(0)->*m)))
+
 namespace CJP
 {
+
 enum SpecialFields
 {
     Name = 1,
@@ -22,7 +25,7 @@ class JSONParser
   public:
     JSONParser()
     {
-        //idMap["SpecialFields>Name"] = 0;
+        idMapping["SpecialFields>Name"] = 0;
     }
 
     // recursion end
@@ -32,7 +35,8 @@ class JSONParser
     void registerType(std::string structName, std::string fieldName, DataT StructT::*member, Rest... rest)
     {
         static_assert(std::is_base_of<Base, StructT>::value, "type parameter of this class must derive from Base");
-        // TODO
+        registerStruct(structName);
+        registerType(structName + ">" + fieldName, customoffsetof(StructT, member));
         registerType(structName, rest...);
     }
 
@@ -40,10 +44,42 @@ class JSONParser
     void registerType(std::string structName, SpecialFields field, DataT StructT::*member, Rest... rest)
     {
         static_assert(std::is_base_of<Base, StructT>::value, "type parameter of this class must derive from Base");
-        //TODO
+        registerStruct(structName);
+        registerType(structName + ">SpecialFields>" + std::to_string(field), customoffsetof(StructT, member));
         registerType(structName, rest...);
     }
 
+    void registerType(std::string typeName, long offset)
+    {
+        idMapping[typeName] = (int)idMapping.size();
+        offsetMapping[idMapping[typeName]] = offset;
+    }
+
+    void registerStruct(std::string structName)
+    {
+        if (idMapping.find(structName) == idMapping.end())
+        {
+            idMapping[structName] = (int)idMapping.size();
+        }
+    }
+
+    void add(std::string name) {
+        Base *p;
+        p = Factory::create("Object");
+
+        // TODO figure out!!!
+
+        // Pointer magic to replace a pointer in the created class
+        long long *address = (long long*)((long long)p + offsetMapping[idMapping["Object>SpecialFields>1"]]);
+        
+        // Overwriting the std::string * 
+        auto test = new std::string("HUHU");
+        *address = (long long)test;
+
+    }
+
   private:
+    std::map<std::string, int> idMapping;
+    std::map<int, long> offsetMapping;
 };
 } // namespace CJP
